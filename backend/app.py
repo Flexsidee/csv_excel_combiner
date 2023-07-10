c903a60236fbb0a9e4a3062de70806904a7ca573
flexsidee <flexsidee@gmail.com>
2023-07-10 20:16:46 +0100

    created an endpoint for sending the contact or feedback as email

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from io import StringIO, BytesIO
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import smtplib
 import csv
 import pandas as pd
 import tempfile
@@ -12,8 +15,16 @@ import datetime
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5173"}})
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Set the temporary directory path
 temp_dir = tempfile.gettempdir()
+
+# get the time and date of email
+now = datetime.datetime.now()
+time = now.strftime("%H:%M")
+day = now.strftime("%d/%m/%Y")
 
 def generate_unique_filename():
     # Generate a random string of characters
@@ -72,6 +83,50 @@ def process_files():
                          mimetype=f'application/{format}')
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    # Get the message data from the request
+    data = request.json
+
+    # Extract the necessary information from the data
+    sender_name = data.get('sender_name')
+    sender_email = data.get('sender_email')
+    message = data.get('message_body')
+
+    # Compose the email to be sent to developer email
+    dev_email_subject = "New message from your CSVExcelCombiner App!"
+    dev_email_body = f"Sender Name: {sender_name}\nSender Email: {sender_email}\nTime: {time} {day}\n\nMessage: {message}"
+
+    # Compose the email to be sent to user email
+    user_email_subject = "CSVExcelCombiner Received your message"
+    user_email_body = f"Hi {sender_name}, \n\nYour message below has been recieved: \n{message}\n\nThank you for feedback and for using CSVExcelCombiner App!\n\nTime: {time} {day}"
+
+    # Email configuration (replace with your own SMTP server details)
+    smtp_host = os.environ.get('SMTP_HOST')
+    smtp_port = os.environ.get('SMTP_PORT')
+    smtp_username = os.environ.get('SMTP_USERNAME')
+    smtp_password = os.environ.get('SMTP_PASSWORD')
+    recipient_email = [ sender_email, os.environ.get('SMTP_USERNAME')]
+
+    subject = [user_email_subject, dev_email_subject]
+    body = [user_email_body, dev_email_body]
+
+    for index, item in enumerate(recipient_email):
+         # Create the email message
+        email = MIMEText(body[index])
+        email['Subject'] = subject[index]
+        email['From'] = smtp_username
+        email['To'] = recipient_email[index]
+
+        # Send the email
+        with smtplib.SMTP(smtp_host, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.send_message(email)
+
+    return 'Email sent!'
 
 
 if __name__ == '__main__':
